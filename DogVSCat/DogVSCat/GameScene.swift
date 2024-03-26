8549b3ccd98621102b17221ff23ece9f697047eb
elohMachado <heloisa.per.machado@gmail.com>
2024-03-26 14:31:54 -0300

    Implementei mecanica basica

--- DogVSCat/DogVSCat/GameScene.swift
+++ DogVSCat/DogVSCat/GameScene.swift
@@ -10,67 +10,86 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var dog: SKSpriteNode!
+    var basket: SKSpriteNode!
     
-    override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+    var collectedItems = 0
+    
+    var itemLabel: SKLabelNode!
+    
+    var itemCount: Int = 0 {
+        didSet {
+            itemLabel.text = "Items: \(itemCount)"
         }
     }
     
+    override func didMove(to view: SKView) {
+        dog = childNode(withName: "dog") as? SKSpriteNode
+        basket = childNode(withName: "basket") as? SKSpriteNode
+        
+        itemLabel = childNode(withName: "items") as? SKLabelNode
+        
+        startItemSpawn()
+        
+    }
+    
+    func startItemSpawn() {
+        let spawnAction = SKAction.run(spawnItem)
+        let delayAction = SKAction.wait(forDuration: 1.5)
+        let sequenceAction = SKAction.sequence([spawnAction, delayAction])
+        let repeatAction = SKAction.repeatForever(sequenceAction)
+        run(repeatAction)
+    }
+    
+    func spawnItem() {
+        let item = SKSpriteNode(imageNamed: "item")
+        item.name = "item"
+        
+        let spawnX = CGFloat.random(in: item.size.width/2...(size.width - item.size.width/2))
+        let spawnY = size.height
+        
+        item.zPosition = 5
+        item.position = CGPoint(x: spawnX, y: spawnY)
+        addChild(item)
+        
+        let moveAction = SKAction.moveTo(y: -300, duration: 5)
+        let removeAction = SKAction.removeFromParent()
+        let sequenceAction = SKAction.sequence([moveAction, removeAction])
+        item.run(sequenceAction)
+        
+        // Set up physics body for collision detection
+        item.physicsBody = SKPhysicsBody(rectangleOf: item.size)
+        item.physicsBody?.categoryBitMask = 1
+        item.physicsBody?.contactTestBitMask = 1
+        item.physicsBody?.collisionBitMask = 0
+        item.physicsBody?.affectedByGravity = true
+    }
     
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+        
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+        
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
         for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        guard let touch = touches.first else { return }
+        let touchLocation = touch.location(in: self)
+        
+        let distance = abs(touchLocation.x - dog.position.x)
+        let duration = TimeInterval(distance / 150)
+        
+        let moveAction = SKAction.moveTo(x: touchLocation.x, duration: duration)
+        dog.run(moveAction)
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -83,6 +102,31 @@ class GameScene: SKScene {
     
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        enumerateChildNodes(withName: "item") { node, _ in
+            if node.frame.intersects(self.dog.frame) {
+                if self.collectedItems < 3 {
+                    node.removeFromParent()
+                    self.collectedItems += 1
+                    self.updateItemLabel()
+                }
+            }
+        }
+        
+        if dog.frame.intersects(basket.frame) && collectedItems > 0 {
+            // Pause dog movement while unloading items
+            dog.removeAllActions()
+            // Simulate unloading time
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                self.collectedItems = 0
+                self.updateItemLabel()
+                self.dog.removeAllActions()
+//                let moveAction = SKAction.moveTo(x: self.size.width / 2, duration: 0.5)
+//                self.dog.run(moveAction)
+            }
+        }
+    }
+    
+    func updateItemLabel() {
+        itemLabel.text = "Items: \(collectedItems)"
     }
 }
